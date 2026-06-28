2a13131102d23fca0e540807d92ecccbc7d2004d
scarlata2805-ops <scarlata2805@gmail.com>
2026-06-28 11:26:47 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='pituspc-v3';
-const FILES=['./','./index.html','./assets/css/styles.css','./assets/js/app.js','./assets/js/productos.js','./assets/images/logo-pituspc-3d.png','./manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
